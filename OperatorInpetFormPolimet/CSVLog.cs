d807cf375b7d47b0ff48e4cd78518368e2e0e525
Виталий <valexms@mail.ru>
2023-05-19 23:36:53 +0300

    Change CSVLog

--- OperatorInpetFormPolimet/CSVLog.cs
+++ OperatorInpetFormPolimet/CSVLog.cs
@@ -12,6 +12,7 @@ namespace OperatorInpetFormPolimet
  
     public  class CSVLog
     {
+        //new comment 20230519
         readonly  string nameFileCSV = "modellog.csv";
         readonly string FullPathToCSVFile;
         readonly string firstString = "Timestamp;Param_1_Optimal;Param_1_Managed;Parama_1_Comment;Param_2_Optimal;Param_2_Managed;Parama_2_Comment;Param_3_Optimal;Param_3_Managed;Parama_3_Comment;Param_4_Optimal;Param_4_Managed;Parama_4_Comment;Param_5_Optimal;Param_5_Managed;Parama_5_Comment";
