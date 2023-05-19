c4d8601410708e1daea42daaff26ca98369b9252
Виталий <valexms@mail.ru>
2023-05-19 23:36:20 +0300

    Add DAL library

---
+++ DAL_Lib/Model/MyEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_Lib.Model
+{
+    public class MyEntity
+    {
+        public string Param1OptValue { get; set; } //Значение параметра предложенного моделью
+        public string Param1Value { get; set; } //Значение параметра установленное оператором
+        public string Param1Comment { get; set; } //Значение комментария для первого параметра
+
+        public string Param2OptValue { get; set; } //Значение параметра предложенного моделью
+        public string Param2Value { get; set; } //Значение параметра установленное оператором
+        public string Param2Comment { get; set; } //Значение комментария для первого параметра
+
+        public string Param3OptValue { get; set; } //Значение параметра предложенного моделью
+        public string Param3Value { get; set; } //Значение параметра установленное оператором
+        public string ParaьmComment { get; set; } //Значение комментария для первого параметра
+
+        public string Param4OptValue { get; set; } //Значение параметра предложенного моделью
+        public string Param4Value { get; set; } //Значение параметра установленное оператором
+        public string Param4Comment { get; set; } //Значение комментария для первого параметра
+
+        public string Param5OptValue { get; set; } //Значение параметра предложенного моделью
+        public string Param5Value { get; set; } //Значение параметра установленное оператором
+        public string Param5Comment { get; set; } //Значение комментария для первого параметра
+    }
+}
